d513e8199f6d1a6ea319bcadf28229f48e99271b
amarenderautomation <amar.email29@gmail.com>
2020-02-24 23:57:23 +0530

    add one line

--- firstgit/src/encpsulation/Encapanotherclass.java
+++ firstgit/src/encpsulation/Encapanotherclass.java
@@ -11,6 +11,8 @@ public class Encapanotherclass
 		System.out.println("Eid="+ob.getEmpid());
 		System.out.println("ename="+ob.getEmpname());
 		System.out.println("essn="+ob.getEmpssn());
+		System.out.println("essn="+ob.getEmpssn());
+
 	}
 
 }
