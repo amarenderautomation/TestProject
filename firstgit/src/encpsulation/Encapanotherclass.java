5ecd0fede77c1034ac3512ad4b1f97ecb7686811
amarenderautomation <61391931+amarenderautomation@users.noreply.github.com>
2021-05-03 00:39:27 +0530

    Update Encapanotherclass.java
    only name changed

--- firstgit/src/encpsulation/Encapanotherclass.java
+++ firstgit/src/encpsulation/Encapanotherclass.java
@@ -5,7 +5,7 @@ public class Encapanotherclass
 	public static void main(String args[]) {
 		Encapsulation ob =new Encapsulation();
 		ob.setEmpid(25);
-		ob.setEmpname("Arun");
+		ob.setEmpname("A");
 		ob.setEmpssn(23569);
 		
 		System.out.println("Eid="+ob.getEmpid());
